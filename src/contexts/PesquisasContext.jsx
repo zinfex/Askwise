c1935e101c93e4a00c3d1868dff4ef1fec1a1003
Arthur Rocha <zinfegr@gmail.com>
2025-01-29 18:10:25 -0300

    fix array pesquisas

--- src/contexts/PesquisasContext.jsx
+++ src/contexts/PesquisasContext.jsx
@@ -10,12 +10,9 @@ export const PesquisaProvider = ({ children }) => {
     setPesquisas([
       {
           "id": 1,
-          "order": "14.00000000000000000000",
-          "nome": "(Final) NPS / Pesquisa de satisfação",
-          "descricao": "Elaborar algumas perguntas para fazer uma pesquisa de satisfação onde, a partir dessas perguntas, chegaremos a idealização concreta do ICP e enxergaremos pontos de melhoras internas.",
-          "data": "16/08/2024 - 09:51:15",
-          "cor": null,
-          "hash_id": "W5fB2g"
+          "nome": "Pesquisa de satisfação",
+          "descricao": "Elaborar algumas perguntas para fazer uma pesquisa de satisfação.",
+          "data": "29/01/2025 - 09:51:15",
       }
   ]);
   }, []);
